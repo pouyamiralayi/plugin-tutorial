b9d817351454a7c311bda22a9ee4222c81b3edeb
sarpedon333 <sarpedon000@gmail.com>
2020-01-21 22:02:51 +0330

    export UI: showing content type table : sublist

--- plugins/import-content/admin/src/components/ContentTypeTable/index.js
+++ plugins/import-content/admin/src/components/ContentTypeTable/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect, useContext} from 'react'
 import PropTypes from 'prop-types'
 import {Checkbox} from '@buffetjs/core'
 // import {List} from '@buffetjs/custom'
@@ -8,94 +8,105 @@ import TableRow from '../TableRow'
 import WrapperList from "./WrapperList";
 import {FormattedMessage} from 'react-intl'
 import getTrad from "../../utils/getTrad";
+import ContentTypeTableSubList from "../ContentTypeTableSubList";
 
 const ContentTypeTable = ({onModelChecked, onModelClicked, models, configs, selectedOption}) => {
 
-    // console.log('modelOptions: ', models);
-    const CustomRow = ({label, uid, checked}) => {
-      // console.log(label, uid, checked, selected);
-      // const {label, uid, checked, selected} = row;
-      return (
-        <TableRow
-          selected={selectedOption === uid}
-          key={uid}
-          onClick={ev => {
-            onModelClicked({uid})
-          }}
-          className={['clickable']}>
-          <a>
-            <span>{label}</span> &nbsp;
-            <Checkbox
-              name={uid}
-              onChange={({target: {value}}) => onModelChecked({uid, checked: value})}
-              value={checked}/>
-          </a>
-        </TableRow>
-      )
-    };
-
-    CustomRow.propTypes = {
-      label: PropTypes.string,
-      uid: PropTypes.string,
-      checked: PropTypes.bool,
-      selected: PropTypes.bool,
-    };
-
-    const props = {
-      title: 'Ready to migrate',
-      subtitle: 'List of content types available for migration',
-      // button: {
-      //   color: 'primary',
-      //   // icon: 'content-type',
-      //   icon: true,
-      //   label: 'Add new Content Type',
-      //   onClick: () => alert('Do you want to create a new chief entry?'),
-      //   type: 'button',
-      // },
-    };
-
-    const renderItem = (m, i) => {
-      return (
-        <CustomRow {...m} key={i}/>
-      )
-    };
-
+  const [search, setSearch] = useState("");
+  const migrations = [
+    {
+      title: "18.3",
+      models: [
+        {uid: 'application::book.book', name: 'book'},
+        {uid: 'application::contact.contact', name: 'contact'}
+      ]
+    }
+  ];
+  // console.log('modelOptions: ', models);
+  const CustomRow = ({label, uid, checked}) => {
+    // console.log(label, uid, checked, selected);
+    // const {label, uid, checked, selected} = row;
     return (
-      <Wrapper>
-        <WrapperList>
-          <div className={'list-header'}>
-            <div className="title-wrapper">
-              {/*<h3 style={{ 'padding-left': '1.2rem'}}>*/}
-              <h3>
-                <FormattedMessage id={getTrad(`menu.section.models.name.${models.length > 1 ? 'plural' : 'singular'}`)}/>
-                &nbsp;&nbsp;
-                <span className="count-info" datadescr={models.length}>
+      <TableRow
+        selected={selectedOption === uid}
+        key={uid}
+        onClick={ev => {
+          onModelClicked({uid})
+        }}
+        className={['clickable']}>
+        <a>
+          <span>{label}</span> &nbsp;
+          <Checkbox
+            name={uid}
+            onChange={({target: {value}}) => onModelChecked({uid, checked: value})}
+            value={checked}/>
+        </a>
+      </TableRow>
+    )
+  };
+
+  CustomRow.propTypes = {
+    label: PropTypes.string,
+    uid: PropTypes.string,
+    checked: PropTypes.bool,
+    selected: PropTypes.bool,
+  };
+
+  const props = {
+    title: 'Ready to migrate',
+    subtitle: 'List of content types available for migration',
+    // button: {
+    //   color: 'primary',
+    //   // icon: 'content-type',
+    //   icon: true,
+    //   label: 'Add new Content Type',
+    //   onClick: () => alert('Do you want to create a new chief entry?'),
+    //   type: 'button',
+    // },
+  };
+
+  const renderItem = (m, i) => {
+    return (
+      <CustomRow {...m} key={i}/>
+    )
+  };
+
+  return (
+    <Wrapper>
+      <WrapperList>
+        <div className={'list-header'}>
+          <div className="title-wrapper">
+            {/*<h3 style={{ 'padding-left': '1.2rem'}}>*/}
+            <h3>
+              <FormattedMessage id={getTrad(`menu.section.models.name.${models.length > 1 ? 'plural' : 'singular'}`)}/>
+              &nbsp;&nbsp;
+              <span className="count-info" datadescr={models.length}>
                 {models.length}
               </span>
-              </h3>
-              {/*<button onClick={toggleSearch}>*/}
-              {/*  <FontAwesomeIcon icon="search"/>*/}
-              {/*</button>*/}
-            </div>
+            </h3>
+            {/*<button onClick={toggleSearch}>*/}
+            {/*  <FontAwesomeIcon icon="search"/>*/}
+            {/*</button>*/}
           </div>
-          <List>{models.map((m, i) => renderItem(m, i))}</List>
-          <div className={'list-header'} style={{marginTop:'1.2rem'}}>
-            <div className="title-wrapper">
-              <h3>
-                <FormattedMessage
-                  id={getTrad(`menu.section.migration.configs.name.${configs.length > 1 ? 'plural' : 'singular'}`)}/>
-                &nbsp;&nbsp;
-                <span className="count-info" datadescr={configs.length}>
+        </div>
+        <List>{models.map((m, i) => renderItem(m, i))}</List>
+        <div className={'list-header'} style={{marginTop: '1.2rem'}}>
+          <div className="title-wrapper">
+            <h3>
+              <FormattedMessage
+                id={getTrad(`menu.section.migration.configs.name.${configs.length > 1 ? 'plural' : 'singular'}`)}/>
+              &nbsp;&nbsp;
+              <span className="count-info" datadescr={configs.length}>
                 {configs.length}
               </span>
-              </h3>
-            </div>
+            </h3>
           </div>
-        </WrapperList>
-      </Wrapper>
-    )
-  }
-;
+        </div>
+        {migrations.map((m, i) => <ContentTypeTableSubList {...m} isFirstItem={i == 0} key={i}/>)}
+      </WrapperList>
+    </Wrapper>
+  )
+};
 
 ContentTypeTable.propTypes = {
   onModelChecked: PropTypes.func,
@@ -106,7 +117,7 @@ ContentTypeTable.propTypes = {
 };
 
 ContentTypeTable.defaultProps = {
-  configs:[]
+  configs: []
 };
 
 export default ContentTypeTable
---
+++ plugins/import-content/admin/src/components/ContentTypeTableSubList/index.js
@@ -0,0 +1,100 @@
+import React, {useEffect, useContext, useState} from 'react';
+import PropTypes from 'prop-types';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import TableRow from "../TableRow";
+import {Checkbox} from '@buffetjs/core'
+import { Collapse } from 'reactstrap';
+import Dropdown from "./DROPDOWN.JS";
+const ContentTypeTableSubList = ({title, models, isFirstItem, isSearching, onClickDelete}) => {
+  const [collapse, setCollapse] = useState(isFirstItem);
+
+  const toggle = () => {
+    setCollapse(!collapse);
+  };
+
+
+  useEffect(() => {
+    if (isSearching) {
+      setCollapse(true);
+    } else {
+      setCollapse(isFirstItem);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isSearching]);
+
+  const CustomRow = ({uid, name}) => {
+    // console.log(label, uid, checked, selected);
+    // const {label, uid, checked, selected} = row;
+    return (
+      <TableRow
+        // selected={selectedOption === uid}
+        key={uid}
+        onClick={ev => {
+          // onModelClicked({uid})
+        }}
+        className={['clickable']}>
+        <a>
+          <span>{name}</span> &nbsp;
+        </a>
+      </TableRow>
+    )
+  };
+
+  CustomRow.propTypes = {
+    label: PropTypes.string,
+    uid: PropTypes.string,
+    checked: PropTypes.bool,
+    selected: PropTypes.bool,
+  };
+
+  return (
+    models.length > 0 && (
+      <Dropdown>
+        <button
+          onClick={toggle}
+          className={`editable ${collapse ? 'is-open' : ''}`}
+        >
+          {title}
+          <FontAwesomeIcon
+            icon={'fa fa-trash'}
+            onClick={e => {
+              onClickDelete(e, {title})
+            }}
+          />
+        </button>
+        <Collapse isOpen={collapse}>
+          <ul>
+            {models.map(m => {
+              const {uid} = m;
+              return (
+                <li key={uid}>
+                  <CustomRow {...m}/>
+                </li>
+              )
+            })}
+          </ul>
+        </Collapse>
+      </Dropdown>
+    )
+  )
+
+};
+
+ContentTypeTableSubList.defaultProps = {
+  title: "",
+  models: [],
+  isFirstItem: false,
+  isSearching: false,
+  onClickDelete: () => {
+  }
+};
+
+ContentTypeTableSubList.propTypes = {
+  title: PropTypes.string,
+  models: PropTypes.array,
+  isFirstItem: PropTypes.bool,
+  isSearching: PropTypes.bool,
+  onClickDelete: PropTypes.func,
+};
+
+export default ContentTypeTableSubList;
